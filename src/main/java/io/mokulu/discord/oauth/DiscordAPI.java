cc032410f53c4370831bd3a4d0698177142e1202
Sky <livegogo.16334@gmail.com>
2022-11-22 20:41:23 +0100

    :rocket: Added supports for joining guilds

--- src/main/java/io/mokulu/discord/oauth/DiscordAPI.java
+++ src/main/java/io/mokulu/discord/oauth/DiscordAPI.java
@@ -1,20 +1,18 @@
 package io.mokulu.discord.oauth;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
-import java.util.List;
-
-import org.jsoup.Jsoup;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
 import io.mokulu.discord.oauth.model.Connection;
 import io.mokulu.discord.oauth.model.Guild;
 import io.mokulu.discord.oauth.model.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -61,6 +59,22 @@ public class DiscordAPI
     public List<Guild> fetchGuilds() throws IOException
     {
         return Arrays.asList(toObject(handleGet("/users/@me/guilds"), Guild[].class));
+    }
+
+    /**
+     * Make a specific user join a guild with the provided ID.
+     * <br>This require the <code>guilds.join</code> scope.
+     * <br>We use <code>PUT</code> method to join the guild.
+     * @param guildId The guild ID to join.
+     * @throws IOException If the request failed somehow.
+     */
+    public void joinGuild(String guildId) throws IOException {
+        final User user = fetchUser();
+        final String path = String.format("/guilds/%s/members/%s", guildId, user.getId());
+        final org.jsoup.Connection request = Jsoup.connect(BASE_URI + path).ignoreContentType(true);
+        setHeaders(request);
+        request.method(org.jsoup.Connection.Method.PUT);
+        request.execute();
     }
 
     public List<Connection> fetchConnections() throws IOException
